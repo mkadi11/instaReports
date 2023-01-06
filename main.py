b32a545649dfc51a3792f5e53553cd620e449f72
mkadi11 <mansi.kadi3@gmail.com>
2023-01-06 15:15:26 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import json
+
+with open('followers.json') as file:
+    followers_json = json.load(file)
+
+with open('following.json') as file:
+    following_json = json.load(file)
+
+following = []
+for f in following_json['relationships_following']:
+    following.append(f['string_list_data'][0]['value'])
+
+for f in followers_json['relationships_followers']:
+    if f['string_list_data'][0]['value'] in following:
+        following.remove(f['string_list_data'][0]['value'])
+
+for account in following:
+    print(account)
+    print()
